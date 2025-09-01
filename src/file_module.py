05a456fc0186909b8916290145334a8900a831fc
VladislavRyabokon <rbibokon@yandex.ru>
2025-09-01 22:01:32 +0700

    fix errors

--- src/file_module.py
+++ src/file_module.py
@@ -117,31 +117,31 @@ class FileChange(WorkIsFile):
                 break
         self.save_file_uniq(self.data_file)
 
-        def open_file(self) -> None:
-            """Метод открытия файла"""
-            if os.path.exists(self.__filename):
-                with open(self.__filename, mode="r", encoding="utf-8") as file:
-                    self.data_file = json.load(file)
-            else:
-                with open(self.__filename, mode="a", encoding="utf-8"):
-                    pass
+    def open_file(self) -> None:
+        """Метод открытия файла"""
+        if os.path.exists(self.__filename):
+            with open(self.__filename, mode="r", encoding="utf-8") as file:
+                self.data_file = json.load(file)
+        else:
+            with open(self.__filename, mode="a", encoding="utf-8"):
+                pass
 
-        def write(self, text: list[dict], file_mode: str) -> None:
-            """Запись в файл"""
-            with open(self.__filename, mode=file_mode, encoding="utf-8") as file:
-                file.write(json.dumps(text, ensure_ascii=False, indent=4))
-            self.data_file = text
+    def write(self, text: list[dict], file_mode: str) -> None:
+        """Запись в файл"""
+        with open(self.__filename, mode=file_mode, encoding="utf-8") as file:
+            file.write(json.dumps(text, ensure_ascii=False, indent=4))
+        self.data_file = text
 
-        def add_vacancy(self, list_vacancy_hh: list[Vacancy]) -> None:
-            """Метод добавления в файл новых вакансий"""
-            list_ids = [values["id_vacancy"] for values in self.data_file]
-            list_vacancy = [vacancy.to_dict() for vacancy in list_vacancy_hh if vacancy.requirement not in list_ids]
-            self.write(list_vacancy, "a")
+    def add_vacancy(self, list_vacancy_hh: list[Vacancy]) -> None:
+        """Метод добавления в файл новых вакансий"""
+        list_ids = [values["id_vacancy"] for values in self.data_file]
+        list_vacancy = [vacancy.to_dict() for vacancy in list_vacancy_hh if vacancy.requirement not in list_ids]
+        self.write(list_vacancy, "a")
 
-        def delete_vacancy(self, vacancy: Vacancy) -> None:
-            """Удаление вакансии"""
-            for index, data in enumerate(self.data_file):
-                if data["id_vacancy"] == vacancy.id_vacancy:
-                    del self.data_file[index]
-                    break
-            self.write(self.data_file, "w")
+    def delete_vacancy(self, vacancy: Vacancy) -> None:
+        """Удаление вакансии"""
+        for index, data in enumerate(self.data_file):
+            if data["id_vacancy"] == vacancy.id_vacancy:
+                del self.data_file[index]
+                break
+        self.write(self.data_file, "w")
